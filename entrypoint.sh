32042020b5affedee93efb0e0b12cf39aed059e4
Jihyun <legojhk@gmail.com>
2024-09-19 16:04:46 +0900

    alembic 설정

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,13 @@ if [ ! -d ./venv/ ]; then \
     source venv/bin/activate && \
     pip install -r requirements.txt && \
     pip freeze > requirements.txt && \
+    alembic upgrade head
     uvicorn app.main:app --host 0.0.0.0 --port 8002 --reload \
 ;else \
     echo "[INFO] >> Ready to luanch server, checking new dependencies, please wait..." && \
     source venv/bin/activate && \
     pip install -r requirements.txt && \
     pip freeze > requirements.txt && \
+    alembic upgrade head
     uvicorn app.main:app --host 0.0.0.0 --port 8002 --reload \
 ;fi
