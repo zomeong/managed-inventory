1fea7c90ba957ef05c598fa6ce8503f1524bdddf
Jihyun <legojhk@gmail.com>
2024-10-14 15:40:11 +0900

    swagger : API 설명 추가

--- app/stock/stock_router.py
+++ app/stock/stock_router.py
@@ -13,7 +13,21 @@ router = APIRouter(
 def get_stock_service(db:Session = Depends(get_db)):
     return StockService(db)
 
-@router.get("/products/{product_code}", response_model=ProductStockResponse)
+@router.get("/products/{product_code}", response_model=ProductStockResponse,
+    description="""
+    특정 물품 재고 조회 API
+
+    Parameter
+    - product_code: 물품 코드
+
+    Output
+    - total_stock: 전체 재고
+    - stock_data: 창고별 재고
+        [
+        - container_name: 창고 이름
+        - quantity: 해당 창고에 저장된 재고
+        ]
+    """)
 @exception_handler
 def get_product_stock(product_code: str, service: StockService = Depends(get_stock_service)):
     stock_data, total_stock = service.get_product_stock(product_code)
@@ -26,7 +40,19 @@ def get_product_stock(product_code: str, service: StockService = Depends(get_sto
         ]
     )
 
-@router.get("/containers/{container_name}", response_model=list[ContainerStockListResponse])
+@router.get("/containers/{container_name}", response_model=list[ContainerStockListResponse],
+    description="""
+    특정 창고 재고 조회 API
+
+    Parameter
+    - container_name: 창고 이름
+
+    Output
+    [
+    - product_code: 물품 코드
+    - quantity: 해당 물품 재고
+    ]
+    """)
 @exception_handler
 def get_container_stock(container_name: str, service: StockService = Depends(get_stock_service)):
     stock_data = service.get_container_stock(container_name)
@@ -36,7 +62,19 @@ def get_container_stock(container_name: str, service: StockService = Depends(get
             for item in stock_data
         ]
 
-@router.get("/products", response_model=list[TotalProductStockResponse])
+@router.get("/products", response_model=list[TotalProductStockResponse],
+    description="""
+    전체 물품 재고 조회 API
+
+    Output
+    - product_code: 물품 코드
+    - total_stock: 물품의 전체 재고
+    - stock_data: 물품의 창고별 재고
+        [
+        - container_name 창고 이름
+        - quantity: 창고에 저장된 재고
+        ]
+    """)
 @exception_handler
 def get_all_products_stock(service: StockService = Depends(get_stock_service)):
     stock_map = service.get_all_products_stock()
@@ -53,7 +91,19 @@ def get_all_products_stock(service: StockService = Depends(get_stock_service)):
         for product_code, stock in stock_map.items()
     ]
 
-@router.get("/containers", response_model=list[TotalContainerStockResponse])
+@router.get("/containers", response_model=list[TotalContainerStockResponse],
+    description="""
+    전체 창고 재고 조회 API
+
+    Output
+    - container_name: 창고 이름
+    - total_stock: 창고의 전체 재고
+    - stock_data: 창고의 물품별 재고
+        [
+        - product_code: 물품 코드
+        - quantity : 해당 물품 재고
+        ]
+    """)
 @exception_handler
 def get_all_containers_stock(service: StockService = Depends(get_stock_service)):
     stock_map = service.get_all_containers_stock()
--- app/transaction/transaction_router.py
+++ app/transaction/transaction_router.py
@@ -13,18 +13,29 @@ router = APIRouter(
 def get_transaction_service(db: Session = Depends(get_db)):
     return TransactionService(db)
 
-@router.post("")
+@router.post("",
+    description="""
+    입출고 기록 추가 API<br>
+    * id: 자동 생성<br>
+    * date: create시 현재 시간으로 자동 할당
+
+    Input
+    - type: 분류(입고/출고)
+    - product_code: 물품 코드
+    - container_name: 창고 이름
+    - quantity: 입/출고 수량
+    """)
 @exception_handler
 def create_transaction(transaction: TransactionCreate, service: TransactionService = Depends(get_transaction_service)):
     service.create_transaction(transaction)
     return "입출고 기록이 추가되었습니다."
 
-@router.get("/{transaction_id}", response_model=TransactionResponse)
+@router.get("/{transaction_id}", response_model=TransactionResponse, description="입출고 기록 조회 API")
 @exception_handler
 def get_transaction(transaction_id: int, service: TransactionService = Depends(get_transaction_service)):
     return service.get_transaction(transaction_id)
 
-@router.get("", response_model=list[TransactionResponse])
+@router.get("", response_model=list[TransactionResponse], description="전체 입출고 기록 조회 API")
 @exception_handler
 def get_all_transactions(service: TransactionService = Depends(get_transaction_service)):
     return service.get_all_transaction()
