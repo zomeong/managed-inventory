30c87544b4faa21d5edcd5c7274d7bc1b0645b16
Jihyun <legojhk@gmail.com>
2024-10-16 17:23:44 +0900

    fix: request schema 오류 fix

--- app/transaction/transaction_schema.py
+++ app/transaction/transaction_schema.py
@@ -7,15 +7,14 @@ class TransactionType(str, Enum):
     OUT = "출고"
 
 class TransactionBase(BaseModel):
-    id: int = Field(alias="_id")
     type: TransactionType
     product_code: str
     container_name: str
     quantity: int = Field(ge=1, description="1개 이상 입/출고 가능합니다.")
-    date: datetime
 
 class TransactionCreate(TransactionBase):
     pass
 
 class TransactionResponse(TransactionBase):
-    pass
+    id: int = Field(alias="_id")
+    date: datetime
